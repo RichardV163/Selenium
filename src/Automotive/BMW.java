7adc7042b1829c538e349c1c565d25d4173c59f7
Hoang Long <hoanglong.vo@mymail.telstra.com>
2018-01-03 10:17:11 +0800

    Commit on 3/1

--- src/Automotive/BMW.java
+++ src/Automotive/BMW.java
@@ -3,16 +3,24 @@ package Automotive;
 import Automotive.Cars;
 
 public class BMW extends Cars {
-    public BMW (int startSpeed){
+    public BMW (int startSpeed) {
         super(startSpeed);
     }
 
     @Override
-    public void increaseSpeed (){
-        System.out.println("Increasing speed BMW");
+    public void increaseSpeed() {
+        protectedSpeed++;
+        Cars c1 = new Cars();
+        c1.decreaseSpeed();
+        System.out.println("Increasing Speed of BMW");
     }
 
-    public void headUpNav(){
-        System.out.println("BMW unique");
+    public void headupDisplayNavigation() {
+        System.out.println("BMW Specific Functionality");
+    }
+
+    public void stop() {
+        Cars c1 = new Cars();
+        c1.decreaseSpeed();
     }
 }
--- src/Automotive/Cars.java
+++ src/Automotive/Cars.java
@@ -2,20 +2,41 @@ package Automotive;
 
 public class Cars {
 
+    // No Modifier - accessible in the same package
     int speed;
-    String brand;
+    // Only in class
+    @SuppressWarnings("unused")
+    private int privateSpeed;
 
-    public Cars(int startSpeed){
-        speed = startSpeed;
+    // Anywhere in Java World with importing the package and class
+    public int publicSpeed;
+
+    // In same package and in sub classes
+    protected int protectedSpeed;
+
+    int speedLimit = 100 ;
+
+    public Cars () {
+        this(0);
     }
 
-    public void increaseSpeed(){
-        speed++;
-        System.out.println("Increase Speed");
+    public void setPrivateSpeed(int pSpeed) {
+        this.privateSpeed = pSpeed;
     }
 
-    public void decreaseSpeed(){
+    public Cars (int startSpeed) {
+        this.privateSpeed = startSpeed;
+    }
+
+    public void increaseSpeed() {
+        if (speed < 100) {
+            speed++;
+        }
+        System.out.println("Increasing Speed of Cars ");
+    }
+
+    protected void decreaseSpeed() {
         speed--;
-        System.out.println("Decrease speed");
+        System.out.println("Decreasing Speed of Cars");
     }
 }
